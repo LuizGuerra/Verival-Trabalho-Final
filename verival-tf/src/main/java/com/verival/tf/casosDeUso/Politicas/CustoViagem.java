bfe5569a704ae98a021373eaa33cf5c5091dbebe
LuizGuerra <luizpedrofg@gmail.com>
2020-06-30 01:18:07 -0300

    CustoViagem bug corrections

--- verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CustoViagem.java
+++ verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CustoViagem.java
@@ -6,13 +6,22 @@ import com.verival.tf.entidades.Roteiro;
 public class CustoViagem {
     private CalculoCustoViagem ccv;
 
-    public CustoViagem(CalculoCustoViagem ccv) {
+    public CustoViagem(CalculoCustoViagem ccv) throws NullPointerException {
+        if (ccv == null) {
+            throw new NullPointerException("Object on initialization cannot be null");
+        }
         this.ccv = ccv;
     }
 
-    public double custoViagem(Roteiro roteiro, Passageiro passageiro) {
+    public double custoViagem(Roteiro roteiro, Passageiro passageiro) throws NullPointerException {
+        if (roteiro == null) {
+            throw new NullPointerException("Null rout instead of declared object");
+        }
+        if (passageiro == null) {
+            throw new NullPointerException("Null passenger instead of declared object");
+        }
         ccv.defineRoteiro(roteiro);
         ccv.definePassageiro(passageiro);
         return ccv.custoViagem();
     }
-}
+} 
