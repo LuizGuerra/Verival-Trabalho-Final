a777c1775b5743bb6e7d819f1a609220107cc523
LuizGuerra <luizpedrofg@gmail.com>
2020-07-02 19:10:43 -0300

    CalculoCustoViagemRelampago refatoration

--- verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CalculoCustoViagemRelampago.java
+++ verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CalculoCustoViagemRelampago.java
@@ -6,27 +6,16 @@ import com.verival.tf.entidades.Roteiro;
 public class CalculoCustoViagemRelampago extends CalculoCustoViagemBasico {
     public CalculoCustoViagemRelampago(Roteiro roteiro, Passageiro passageiro) throws NullPointerException {
         super(roteiro, passageiro);
-        
     }
 
     @Override
     public double descontoPontuacao() {
-        double custobasico = calculoCustoBasico();
-        if (getPassageiro().getPontuacaoMedia() > 5.0 && getPassageiro().getQtdadeAvaliacoes() > 30) {
-            return custobasico * 0.05;
-        } else {
-            return 0.0;
-        }
+        Passageiro p = getPassageiro();
+        return (p.getQtdadeAvaliacoes() > 30 && p.getPontuacaoMedia() > 5) ? calculoCustoBasico()*0.05 : 0;
     }
 
     @Override
     public double descontoPromocaoSazonal() {
-        int qtdadeBairros = getRoteiro().bairrosPercorridos().size();
-        double cb = calculoCustoBasico();
-        if (qtdadeBairros > 3) {
-            return cb * 0.05;
-        } else {
-            return 0.0;
-        }
+        return getRoteiro().bairrosPercorridos().size() > 3 ? calculoCustoBasico() * 0.05 : 0;
     }
 }
