87d5e7bb7bfef4f2209a75c39cd81ae873c8f85a
LuizGuerra <luizpedrofg@gmail.com>
2020-07-01 21:34:23 -0300

    CalculoCustoViagemVerao initializer created

--- verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CalculoCustoViagemVerao.java
+++ verival-tf/src/main/java/com/verival/tf/casosDeUso/Politicas/CalculoCustoViagemVerao.java
@@ -1,6 +1,13 @@
 package com.verival.tf.casosDeUso.Politicas;
 
+import com.verival.tf.entidades.Passageiro;
+import com.verival.tf.entidades.Roteiro;
+
 public class CalculoCustoViagemVerao extends CalculoCustoViagemBasico {
+    public CalculoCustoViagemVerao(Roteiro roteiro, Passageiro passageiro) throws NullPointerException {
+        super(roteiro, passageiro);
+    }
+
     @Override
     public double descontoPontuacao() {
         double custobasico = calculoCustoBasico();
